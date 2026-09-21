29db0c3a85412644c02b52f95de4a23da8a813f8
minhtran123hehe-png <minhtran123hehe@gmail.com>
2026-09-22 00:02:11 +0700

    Add mirrored public catalogue data

---
+++ catalogue.js
@@ -0,0 +1,26 @@
+window.ANTIN_PRODUCTS = [
+  {
+    productId: "1146",
+    name: "Klenzit-C Gel",
+    brand: "Glenmark",
+    category: "Thuốc da liễu",
+    spec: "Tuýp 15g",
+    price: "108.400đ",
+    active: "Adapalene 1 mg/g + Clindamycin 10 mg/g",
+    indication: "Thông tin tham khảo về sản phẩm. Vui lòng dùng theo hướng dẫn chuyên môn.",
+    image: "images/klenzit-c-15g.jpg",
+    visible: true
+  },
+  {
+    productId: "1505",
+    name: "Nexium MUPS 40mg",
+    brand: "AstraZeneca",
+    category: "Thuốc tiêu hóa",
+    spec: "Hộp 14 viên",
+    price: "322.000đ",
+    active: "Esomeprazole 40mg",
+    indication: "Thông tin tham khảo về sản phẩm. Vui lòng dùng theo hướng dẫn chuyên môn.",
+    image: "images/nexium-mups-40mg.jpg",
+    visible: true
+  }
+];
